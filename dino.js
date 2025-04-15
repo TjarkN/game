f7740f8c98853ffb3d53e6ca79432aae98d6d740
Tjark <github@tja-rk.de>
2025-04-15 16:27:23 +0200

    set to hdpi

--- dino.js
+++ dino.js
@@ -88,7 +88,7 @@ var DEFAULT_WIDTH = 600;
 var FPS = 60;
 
 /** @const */
-var IS_HIDPI = window.devicePixelRatio > 1;
+var IS_HIDPI = true;//window.devicePixelRatio > 1;
 
 /** @const */
 var IS_IOS = /iPad|iPhone|iPod/.test(window.navigator.platform);
